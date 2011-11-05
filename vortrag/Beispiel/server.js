0a436d163b1dacbcbd202768d1ed2b45321081dd
Rico P Mac <mailtorico@googlemail.com>
2011-11-05 14:33:35 +0100

    Synchro fertig

--- vortrag/Beispiel/server.js
+++ vortrag/Beispiel/server.js
@@ -1,8 +1,10 @@
-var app = require('http').createServer(handler), io = require('socket.io').listen(app), fs = require('fs');
+var app = require('http').createServer(handler), //
+io = require('socket.io').listen(app), //
+fs = require('fs');
 
 app.listen(8008);
 
-console.log("SE2 DEmo auf Port 8008.")
+console.log("SE2 Demo auf Port 8008.")
 
 function handler(req, res) {
 	fs.readFile(__dirname + '/index.html', function(err, data) {
@@ -17,11 +19,24 @@ function handler(req, res) {
 	});
 }
 
+var sessions = [];
+
 io.sockets.on('connection', function(socket) {
-	socket.emit('news', {
-		hello : 'world'
-	});
-	socket.on('my other event', function(data) {
-		console.log(data);
-	});
+	sessions.push(socket);
+
+	broadcast(socket, sessions, "drag");
+	broadcast(socket, sessions, "new");
+	broadcast(socket, sessions, "kill");
+	broadcast(socket, sessions, "message"); 
 });
+
+function broadcast(socket, sessions, event) {
+	socket.on(event, function(data) {
+		for(var i = 0; i != sessions.length; i++) {
+			if(socket !== sessions[i]) {
+				var s = sessions[i];
+				s.emit(event, data);
+			}
+		}
+	});
+}
